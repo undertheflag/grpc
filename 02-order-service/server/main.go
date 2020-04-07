85c0f8b21fe3bb87c1ecc96d4de93a714404d28a
zjx <zjx@yihaohulian.com>
2020-04-08 01:11:06 +0800

    order-mgt grpc server

---
+++ 02-order-service/server/main.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/golang/protobuf/ptypes/wrappers"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	pb "grpc/ordermgt/server/ecommerce"
+	"io"
+	"log"
+	"net"
+	"strings"
+)
+
+const (
+	port = ":50051"
+	orderBatchSize = 3
+)
+
+var orderMap = make(map[string]pb.Order)
+
+type server struct {
+	orderMap map[string]*pb.Order //命名暴露了底层实现，直接用orders更好
+}
+
+func (s *server) AddOrder(ctx context.Context, o *pb.Order) (*wrappers.StringValue, error) {
+	log.Printf("Order Added. ID : %v", o.Id)
+	orderMap[o.Id] = *o
+	return &wrappers.StringValue{Value: "Order Added: " + o.Id}, nil
+}
+
+func (s *server) GetOrder(ctx context.Context, orderId *wrappers.StringValue) (*pb.Order, error) {
+	ord, exists := orderMap[orderId.Value]
+	if exists {
+		return &ord, status.New(codes.OK, "").Err()
+	}
+
+	return nil, status.Errorf(codes.NotFound, "Order does not exist. : ", orderId)
+}
+
+func (s *server) SearchOrders(searchQuery *wrappers.StringValue, stream pb.OrderManagement_SearchOrdersServer) error {
+	for key, order := range orderMap {
+		log.Print(key, order)
+
+		for _, itemStr := range order.Items {
+			log.Print(itemStr)
+
+			if strings.Contains(itemStr, searchQuery.Value) {
+				err := stream.Send(&order)
+				if err != nil {
+					return fmt.Errorf("error sending message to stream : %v ", err)
+				}
+				log.Print("Matching Order Found : " + key)
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (s *server) UpdateOrders(stream pb.OrderManagement_UpdateOrdersServer) error {
+	ordersStr := "Updated Order IDs : "
+	for {
+		order, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&wrappers.StringValue{Value: "Orders processed " + ordersStr})
+		}
+		if err != nil {
+			return err
+		}
+
+		orderMap[order.Id] = *order
+		log.Printf("Order ID : %s - %s", order.Id, "Updated")
+		ordersStr += order.Id + ", "
+	}
+}
+
+func (s server) ProcessOrders(stream pb.OrderManagement_ProcessOrdersServer) error {
+	batchMarker := 1
+	var combinedShipmentMap = make(map[string]pb.CombinedShipment) // 地址->发货订单记录
+
+	for {
+		orderId, err := stream.Recv()
+		log.Printf("reading Proc order : %s ", orderId)
+		if err == io.EOF {
+			log.Printf("EOF : %s", orderId)
+			for _, shipment := range combinedShipmentMap {
+				if err := stream.Send(&shipment); err != nil {
+					return err
+				}
+			}
+			return nil
+		}
+		if err != nil {
+			log.Println(err)
+			return err
+		}
+
+		destination := orderMap[orderId.GetValue()].Destination //获取订单收货地址
+		//该地址是否已经有发货记录
+		shipment, found := combinedShipmentMap[destination]
+		if found {
+			ord := orderMap[orderId.GetValue()]
+			shipment.OrdersList = append(shipment.OrdersList, &ord)
+			combinedShipmentMap[destination] = shipment
+		} else {
+			comShip := pb.CombinedShipment{
+				Id:     "cmb - " + (orderMap[orderId.GetValue()].Destination),
+				Status: "Processed!",
+			}
+			ord := orderMap[orderId.GetValue()]
+			comShip.OrdersList = append(shipment.OrdersList, &ord)
+			combinedShipmentMap[destination] = comShip
+			log.Print(len(comShip.OrdersList), comShip.GetId())
+		}
+
+		if batchMarker == orderBatchSize {
+			for _, comb := range combinedShipmentMap {
+				log.Printf("Shipping : %v -> %v", comb.Id, len(comb.OrdersList))
+				if err := stream.Send(&comb); err != nil {
+					return err
+				}
+			}
+			batchMarker = 0
+			combinedShipmentMap = make(map[string]pb.CombinedShipment)
+		} else {
+			batchMarker++
+		}
+	}
+}
+
+func main() {
+	initSampleData()
+
+	s := grpc.NewServer() //构造gRPC服务对象
+
+	pb.RegisterOrderManagementServer(s, &server{}) //注册服务
+
+	//监听端口,提供gRPC服务
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen %v", err)
+	}
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func initSampleData() {
+	orderMap["102"] = pb.Order{Id: "102", Items: []string{"Google Pixel 3A", "Mac Book Pro"}, Destination: "Mountain View, CA", Price: 1800.00}
+	orderMap["103"] = pb.Order{Id: "103", Items: []string{"Apple Watch S4"}, Destination: "San Jose, CA", Price: 400.00}
+	orderMap["104"] = pb.Order{Id: "104", Items: []string{"Google Home Mini", "Google Nest Hub"}, Destination: "Mountain View, CA", Price: 400.00}
+	orderMap["105"] = pb.Order{Id: "105", Items: []string{"Amazon Echo"}, Destination: "San Jose, CA", Price: 30.00}
+	orderMap["106"] = pb.Order{Id: "106", Items: []string{"Amazon Echo", "Apple iPhone XS"}, Destination: "Mountain View, CA", Price: 300.00}
+}
